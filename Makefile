intel/numatop

commit 2956b875fc1e52e95eb8cc8c1f4ec2c9b4f3fc81

mgen: Reuse functions instead of redefining them
... so that we don't need to maintain the same code twice.

Currently, we compile the selftest by 'cd test/mgen/' and run 'make'
there. Discard this method. Instead, use 'make test' from numatop
home directory to build mgen selftest. This change is needed to reuse
already available functions into mgen selftest. Also, Use 'make numatop'
to build the numatop tool; this won't build the mgen selftest. Run
'make' (or 'make all') to build both - the numatop tool and the mgen
selftest. Mark this change in test/mgen/README. Also, add mgen binary
into .gitignore file.

Signed-off-by: Ravi Bangoria <ravi.bangoria@linux.vnet.ibm.com>

Makefile
@@ -8,11 +8,13 @@ CFLAGS = -g -Wall -O2
 LDFLAGS = -g
 LDLIBS = -lncurses -lpthread -lnuma
 
-COMMON_OBJS = cmd.o disp.o lwp.o numatop.o page.o perf.o \
-	proc.o reg.o util.o win.o ui_perf_map.o
+NUMATOP_OBJS = numatop.o
 
-OS_OBJS = os_cmd.o os_perf.o os_win.o node.o map.o \
-	os_util.o plat.o pfwrapper.o sym.o os_page.o
+COMMON_OBJS = cmd.o disp.o lwp.o page.o perf.o proc.o reg.o util.o \
+	win.o ui_perf_map.o
+
+OS_OBJS = os_cmd.o os_perf.o os_win.o node.o map.o os_util.o plat.o \
+	pfwrapper.o sym.o os_page.o
 
 ARCH := $(shell uname -m)
 
@@ -27,19 +29,32 @@ ARCH_OBJS = $(ARCH_PATH)/wsm.o $(ARCH_PATH)/snb.o $(ARCH_PATH)/nhm.o \
 	$(ARCH_PATH)/util.o $(ARCH_PATH)/ui_perf_map.o
 endif
 
-all: $(PROG)
+TEST_PATH = ./test/mgen
+TEST_PROG = $(TEST_PATH)/mgen
+TEST_OBJS = $(TEST_PATH)/mgen.o
 
-$(PROG): $(COMMON_OBJS) $(OS_OBJS) $(ARCH_OBJS)
-	$(LD) $(LDFLAGS) -o $@ $(COMMON_OBJS) $(OS_OBJS) $(ARCH_OBJS) $(LDLIBS)
-
-%.o: ./common/%.c ./common/include/*.h ./common/include/os/*.h
+%.o: ./common/%.c
 	$(CC) $(CFLAGS) -o $@ -c $<
 
-%.o: ./common/os/%.c ./common/include/*.h ./common/include/os/*.h
+%.o: ./common/os/%.c
 	$(CC) $(CFLAGS) -o $@ -c $<
 
-$(ARCH_PATH)/%.o: $(ARCH_PATH)/%.c $(ARCH_PATH)/include/*.h
+$(ARCH_PATH)/%.o: $(ARCH_PATH)/%.c
 	$(CC) $(CFLAGS) -o $@ -c $<
+
+all: $(PROG) test
+
+# build numatop tool
+$(PROG): $(NUMATOP_OBJS) $(COMMON_OBJS) $(OS_OBJS) $(ARCH_OBJS)
+	$(LD) $(LDFLAGS) -o $@ $(NUMATOP_OBJS) $(COMMON_OBJS) $(OS_OBJS) \
+	$(ARCH_OBJS) $(LDLIBS)
+
+# build mgen selftest
+test: $(TEST_PROG)
+
+$(TEST_PROG): $(TEST_OBJS) $(COMMON_OBJS) $(OS_OBJS) $(ARCH_OBJS)
+	$(LD) $(LDFLAGS) -o $@ $(TEST_OBJS) $(COMMON_OBJS) $(OS_OBJS) \
+	$(ARCH_OBJS) $(LDLIBS)
 
 install: $(PROG)
 	install -m 0755 $(PROG) $(PREFIXDIR)$(BINDIR)/
@@ -47,4 +62,4 @@ install: $(PROG)
 	mv -f numatop.8.gz $(MANDIR)/
 
 clean:
-	rm -rf *.o $(ARCH_PATH)/*.o $(PROG)
+	rm -rf *.o $(ARCH_PATH)/*.o $(TEST_PATH)/*.o $(PROG) $(TEST_PROG)

test/mgen/Makefile
@@ -1,19 +1,0 @@
-PROG = mgen
-CC = gcc
-LD = gcc
-CFLAGS = -g -Wall
-LDLIBS = -lpthread
-
-OBJS = mgen.o
-
-all: $(PROG)
-
-$(PROG): $(OBJS)
-	$(LD) $(LDLIBS) -o $@ $(OBJS) -lnuma
-
-%.o: %.c
-	$(CC) $(CFLAGS) -o $@ -c $<
-
-clean:
-	rm -rf *.o mgen
-	

.gitignore
@@ -3,6 +3,7 @@
 *.patch
 
 /numatop
+/test/mgen/mgen
 
 /tags
 /TAGS

test/mgen/README
@@ -1,9 +1,11 @@
 Building
 ---------
 1. To build mgen
-	make
+	cd <numatop home>/
+	make test
 
 2. To clean the built objects
+	cd <numatop home>/
 	make clean
 
 Note

test/mgen/mgen.c
@@ -55,6 +55,8 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <numa.h>
+#include "../../common/include/util.h"
+#include "../../common/include/os/os_util.h"
 
 #ifndef PATH_MAX
 #define	PATH_MAX	4096
@@ -78,10 +80,10 @@
 #define BUF_SIZE	256 * 1024 * 1024
 #define RAND_ARRAY_SIZE	8192
 #define INVALID_RAND	-1
-#define LINE_SIZE	64
+#define BUF_ELE_SIZE	64
 #define READ_NUM	10240000
 
-static double s_nsofclk, s_clkofns;
+static double s_nsofclk;
 static uint64_t s_clkofsec;
 static int s_rand_arr[RAND_ARRAY_SIZE];
 static double s_latest_avglat = 0.0;
@@ -93,7 +95,6 @@ static unsigned int s_randseed;
 static void *buf_create(int);
 static void buf_release(void *);
 static int dependent_read(void *, int, int, int);
-static uint64_t rdtsc();
 
 static void
 print_usage(const char *exec_name)
@@ -116,218 +117,6 @@ print_usage(const char *exec_name)
 	    "    2. Generate RMA for 10s (memory allocated on node0, thread runs on cpu10):\n"
 	    "       %s -a 0 -c 10 -t 10\n",
 	    basename(buffer), basename(buffer));
-}
-
-/*
- * Get the interval in milliseconds between 2 timestamps.
- */
-static unsigned int
-msdiff(struct timeval *tva, struct timeval *tvb)
-{
-	time_t sdiff = tva->tv_sec - tvb->tv_sec;
-	suseconds_t udiff = tva->tv_usec - tvb->tv_usec;
-
-	if (sdiff < 0) {
-		return (0);
-	}
-
-	if (udiff < 0) {
-		udiff += MICROSEC;
-		sdiff--;
-	}
-
-	if (sdiff < 0) {
-		return (0);
-	}
-
-	if (sdiff >= (MAX_VALUE / MS_SEC)) {
-		return ((unsigned int)MAX_VALUE);
-	}
-
-	return ((unsigned int)(sdiff * MS_SEC + udiff / MS_SEC));
-}
-
-/*
- * Get the current timestamp and convert it to milliseconds
- */
-uint64_t
-current_ms(struct timeval *tvbase)
-{
-	struct timeval tvnow;
-
-	(void) gettimeofday(&tvnow, 0);
-	return (msdiff(&tvnow, tvbase));
-}
-
-/*
- * Bind current thread to a cpu or unbind current thread
- * from a cpu.
- */
-static int
-processor_bind(int cpu)
-{
-	cpu_set_t cs;
-
-	CPU_ZERO (&cs);
-	CPU_SET (cpu, &cs);
-
-	if (sched_setaffinity(0, sizeof (cs), &cs) < 0) {
-		return (-1);
-	}
-
-	return (0);
-}
-
-static int
-processor_unbind(void)
-{
-	cpu_set_t cs;
-	int i;
-
-	CPU_ZERO (&cs);
-	for (i = 0; i < s_ncpus; i++) {
-		CPU_SET (i, &cs);
-	}
-
-	if (sched_setaffinity(0, sizeof (cs), &cs) < 0) {
-		return (-1);
-	}
-
-	return (0);
-}
-
-/*
- * Check the cpu name in proc info. Intel CPUs always have @ x.y
- * Ghz and that is the TSC frequency.
- */
-static int
-calibrate_cpuinfo(double *nsofclk, double *clkofns, uint64_t *clkofsec)
-{
-	FILE *f;
-	char *line = NULL, unit[11];
-	size_t len = 0;
-	double freq = 0.0;
-
-	if ((f = fopen(CPUINFO_PATH, "r")) == NULL) {
-		return (-1);
-	}
-
-	while (getline(&line, &len, f) > 0) {
-		if (strncmp(line, "model name", sizeof ("model name") - 1) != 0) {
-			continue;
-		}
-
-		if (sscanf(line + strcspn(line, "@") + 1, "%lf%10s",
-			&freq, unit) == 2) {
-			if (strcasecmp(unit, "GHz") == 0) {
-				freq *= GHZ;
-			} else if (strcasecmp(unit, "Mhz") == 0) {
-				freq *= MHZ;				
-			}
-			break;
-		}
-	}
-
-	free(line);
-	fclose(f);
-
-	if (freq == 0.0) {
-		return (-1);
-	}
-
-	*clkofsec = freq;
-	*nsofclk = (double)NS_SEC / *clkofsec;
-	*clkofns = (double)1.0 / *nsofclk;
-	return (0);
-}
-
-/*
- * On all recent Intel CPUs, the TSC frequency is always
- * the highest p-state. So get that frequency from sysfs.
- * e.g. 2262000
- */
-static int
-calibrate_cpufreq(double *nsofclk, double *clkofns, uint64_t *clkofsec)
-{
-	int fd, i;
-	char buf[32];
-	uint64_t freq;
-
-	if ((fd = open(CPU0_CPUFREQ_PATH, O_RDONLY)) < 0) {
-		return (-1);
-	}
-
-	if ((i = read(fd, buf, sizeof (buf) - 1)) <= 0) {
-		close(fd);
-		return (-1);
-	}
-
-	close(fd);
-	buf[i] = 0;
-	if ((freq = atoll(buf)) == 0) {
-		return (-1);
-	}
-
-	*clkofsec = freq * KHZ;
-	*nsofclk = (double)NS_SEC / *clkofsec;
-	*clkofns = (double)1.0 / *nsofclk;	
-	return (0);
-}
-
-/*
- * Measure how many TSC cycles in a second and how many nanoseconds
- * in a TSC cycle.
- */
-static void
-calibrate_by_tsc(double *nsofclk, double *clkofns, uint64_t *clkofsec)
-{
-	uint64_t start_ms, end_ms, diff_ms;
-	uint64_t start_tsc, end_tsc;
-
-	/*
-	 * Bind current thread to cpu0 to ensure the
-	 * thread will not be migrated to another cpu
-	 * while the rdtsc runs.
-	 */
-	if (processor_bind(0) != 0) {
-		return;
-	}
-
-	/*
-	 * Make sure the start_ms is at the beginning of
-	 * one millisecond.
-	 */
-	end_ms = current_ms(&s_tvbase);
-	while ((start_ms = current_ms(&s_tvbase)) == end_ms) {}
-
-	start_tsc = rdtsc();
-	while ((end_ms = current_ms(&s_tvbase)) < (start_ms + 100)) {}
-	end_tsc = rdtsc();
-
-	diff_ms = end_ms - start_ms;
-	*nsofclk = (double)(diff_ms * NS_MS) /
-	    (double)(end_tsc - start_tsc);
-	*clkofns = (double)1.0 / *nsofclk;	
-	*clkofsec = (uint64_t)((double)NS_SEC / *nsofclk);
-
-	/*
-	 * Unbind current thread from cpu0 once the measurement completed.
-	 */
-	(void) processor_unbind();
-}
-
-void
-calibrate(void)
-{
-	if (calibrate_cpuinfo(&s_nsofclk, &s_clkofns, &s_clkofsec) == 0) {
-		return;
-	}
-	
-	if (calibrate_cpufreq(&s_nsofclk, &s_clkofns, &s_clkofsec) == 0) {
-		return;	
-	}
-
-	calibrate_by_tsc(&s_nsofclk, &s_clkofns, &s_clkofsec);
 }
 
 static void
@@ -441,7 +230,7 @@ main(int argc, char *argv[])
 	}
 
 	gettimeofday(&s_tvbase, 0);
-	calibrate();
+	os_calibrate(&s_nsofclk, &s_clkofsec);
 	
 	if ((s_buf = buf_create(node_alloc)) == NULL) {
 		printf("Failed to create buffer.\n");
@@ -523,7 +312,7 @@ rand_array_init(void)
 static void
 rand_buf_init(void *buf, int size)
 {
-	int nblk = size / (RAND_ARRAY_SIZE * LINE_SIZE);
+	int nblk = size / (RAND_ARRAY_SIZE * BUF_ELE_SIZE);
 	int i, j;
 	uint64_t **p, **blk_start, **end = NULL;
 	
@@ -535,8 +324,8 @@ rand_buf_init(void *buf, int size)
 				end = p;
 			}
 
-			*p = (uint64_t *)((char *)blk_start + (s_rand_arr[j] * LINE_SIZE));
-			p = (uint64_t **)((char *)p + LINE_SIZE);
+			*p = (uint64_t *)((char *)blk_start + (s_rand_arr[j] * BUF_ELE_SIZE));
+			p = (uint64_t **)((char *)p + BUF_ELE_SIZE);
 		}
 	}
 	
@@ -568,20 +357,6 @@ static void
 buf_release(void *buf)
 {
 	numa_free(buf, BUF_SIZE);	
-}
-
-static uint64_t
-rdtsc()
-{	
-	uint64_t var;
-	uint32_t hi, lo;
-
-	__asm volatile
-	    ("rdtsc" : "=a" (lo), "=d" (hi));
-
-	/* LINTED E_VAR_USED_BEFORE_SET */
-	var = ((uint64_t)hi << 32) | lo;
-	return (var);
 }
 
 static
@@ -618,7 +393,7 @@ dependent_read(void *buf, int cpu_consumer, int node_alloc, int meas_sec)
 	uint64_t run_cyc, total_cyc, dur_cyc;
 
 	run_cyc = (uint64_t)((uint64_t)meas_sec * 
-	    (uint64_t)((double)(NS_SEC) * s_clkofns));
+	    (uint64_t)((double)(NS_SEC) * ((double)1.0 / s_nsofclk)));
 	    
 	if (processor_bind(cpu_consumer) != 0) {
 		return (-1);

commit 9a04e3a0c61367c264238a054608dc1db169263e

mgen: Compile without optimization
Compile mgen selftest without optimization. This was already the case
until I changed method to compile mgen.

Fixes: 2956b875fc1e ("mgen: Reuse functions instead of redefining them")
Signed-off-by: Ravi Bangoria <ravi.bangoria@linux.vnet.ibm.com>

Makefile
@@ -5,6 +5,7 @@ PROG = numatop
 CC = gcc
 LD = gcc
 CFLAGS = -g -Wall -O2
+TEST_CFLAGS = -g -Wall -O0
 LDFLAGS = -g
 LDLIBS = -lncurses -lpthread -lnuma
 
@@ -48,8 +49,11 @@ TEST_ARCH_OBJS = $(TEST_ARCH_PATH)/util.o
 $(ARCH_PATH)/%.o: $(ARCH_PATH)/%.c
 	$(CC) $(CFLAGS) -o $@ -c $<
 
+$(TEST_PATH)/%.o: $(TEST_PATH)/%.c
+	$(CC) $(TEST_CFLAGS) -o $@ -c $<
+
 $(TEST_ARCH_PATH)/%o: $(TEST_ARCH_PATH)/%.c
-	$(CC) $(CFLAGS) -o $@ -c $<
+	$(CC) $(TEST_CFLAGS) -o $@ -c $<
 
 all: $(PROG) test
 