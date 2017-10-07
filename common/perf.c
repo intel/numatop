intel/numatop

commit eb574e79c205bdb71479e815a465874bc025ee6a

Support multiple perf event for one UI parameter
Right now, numatop assumes one event for one UI parameter and uses
same enum 'count_id_t' for both user-frontend and perf-backend. But
this approach has limitation; when one UI parameter needs data from
more than one perf events, it can't be achieved.

For ex,
Power8 processors can be either SCM (Single Chip Modules - one chip
in one socket) or DCM (Dual Chip Modules - two chips in one socket).
Memory access on SCM is either local(LMEM) or distant(DMEM). But on
DCM, it can be remote memory(RMEM) access as well, which is memory
access in the same socket but different chip. Thus, we need to
consider both RMEM and DMEM to get accurate RMA count.

This can be achieved by adding a layer between perf-backend and user-
frontend. The layer will transform UI parameter to perf event and
vice versa.

Introduce new data structures 'perf_count_id_t' and 'ui_count_id_t'
and add middle layer 'ui_perf_count_map_t' to map between them. The
middle layer will contain logic about how to transform ui parameter
to perf events and how to aggregate data recorded by perf-backend
and pass it to user-frontend.

    User frontend    |       middle layer     |    perf backend
    =================|========================|===================
                     |                        |
    ui_count_id_t    |   ui_perf_count_map_t  |    perf_count_id_t
                     |                        |
    Ui parameter ----+-------> Lookup --------+--> perf events
                     |                        |         |
                     |                        |         v
    Data shown   <---+------- Aggregate  <----+--- recorded data
    to User          |                        |    by perf
                     |                        |

Signed-off-by: Ravi Bangoria <ravi.bangoria@linux.vnet.ibm.com>

common/perf.c
@@ -41,13 +41,14 @@
 #include "include/lwp.h"
 #include "include/util.h"
 #include "include/disp.h"
+#include "include/ui_perf_map.h"
 #include "include/os/plat.h"
 #include "include/os/node.h"
 #include "include/os/os_perf.h"
 
 static perf_ctl_t s_perf_ctl;
 
-uint64_t g_sample_period[COUNT_NUM][PRECISE_NUM] = {
+uint64_t g_sample_period[PERF_COUNT_NUM][PRECISE_NUM] = {
 	{ SMPL_PERIOD_CORECLK_DEFAULT,
 	  SMPL_PERIOD_CORECLK_MIN,
 	  SMPL_PERIOD_CORECLK_MAX
@@ -67,7 +68,13 @@ uint64_t g_sample_period[COUNT_NUM][PRECISE_NUM] = {
 	{ SMPL_PERIOD_LMA_DEFAULT,
 	  SMPL_PERIOD_LMA_MIN,
 	  SMPL_PERIOD_LMA_MAX
+	},
+#ifdef __powerpc64__
+	{ SMPL_PERIOD_RMA_1_DEFAULT,
+	  SMPL_PERIOD_RMA_1_MIN,
+	  SMPL_PERIOD_RMA_1_MAX
 	}
+#endif
 };
 
 typedef struct _perf_pqos_arg {
@@ -87,7 +94,11 @@ task_valid(perf_task_t *task)
 		/* fall through */
 	case PERF_PROFILING_PARTPAUSE_ID:
 		/* fall through */
+	case PERF_PROFILING_MULTIPAUSE_ID:
+		/* fall through */
 	case PERF_PROFILING_RESTORE_ID:
+		/* fall through */
+	case PERF_PROFILING_MULTI_RESTORE_ID:
 		/* fall through */
 	case PERF_LL_START_ID:
 		/* fall through */
@@ -239,11 +250,19 @@ perf_handler(void *arg)
 		case PERF_PROFILING_PARTPAUSE_ID:
 			(void) os_profiling_partpause(&s_perf_ctl, &task);
 			break;
-			
+
+		case PERF_PROFILING_MULTIPAUSE_ID:
+			(void) os_profiling_multipause(&s_perf_ctl, &task);
+			break;
+
 		case PERF_PROFILING_RESTORE_ID:
 			(void) os_profiling_restore(&s_perf_ctl, &task);
 			break;
-		
+
+		case PERF_PROFILING_MULTI_RESTORE_ID:
+			(void) os_profiling_multi_restore(&s_perf_ctl, &task);
+			break;
+
 		case PERF_CALLCHAIN_START_ID:
 			(void) os_callchain_start(&s_perf_ctl, &task);
 			break;
@@ -474,15 +493,31 @@ perf_profiling_smpl(boolean_t use_dispflag1)
 }
 
 int
-perf_profiling_partpause(count_id_t count_id)
+perf_profiling_partpause(ui_count_id_t ui_count_id)
 {
-	return (os_perf_profiling_partpause(count_id));
+	perf_count_id_t *perf_count_ids = NULL;
+	int n_perf_count;
+
+	n_perf_count = get_ui_perf_count_map(ui_count_id, &perf_count_ids);
+	if (n_perf_count > 1) {
+		return (os_perf_profiling_multipause(perf_count_ids));
+	} else {
+		return (os_perf_profiling_partpause(perf_count_ids[0]));
+	}
 }
 
 int
-perf_profiling_restore(count_id_t count_id)
+perf_profiling_restore(ui_count_id_t ui_count_id)
 {
-	return (os_perf_profiling_restore(count_id));
+	perf_count_id_t *perf_count_ids = NULL;
+	int n_perf_count;
+
+	n_perf_count = get_ui_perf_count_map(ui_count_id, &perf_count_ids);
+	if (n_perf_count > 1) {
+		return (os_perf_profiling_multi_restore(perf_count_ids));
+	} else  {
+		return (os_perf_profiling_restore(perf_count_ids[0]));
+	}
 }
 
 boolean_t

Makefile
@@ -9,7 +9,7 @@ LDFLAGS = -g
 LDLIBS = -lncurses -lpthread -lnuma
 
 COMMON_OBJS = cmd.o disp.o lwp.o numatop.o page.o perf.o \
-	proc.o reg.o util.o win.o
+	proc.o reg.o util.o win.o ui_perf_map.o
 
 OS_OBJS = os_cmd.o os_perf.o os_win.o node.o map.o \
 	os_util.o plat.o pfwrapper.o sym.o os_page.o
@@ -18,12 +18,13 @@ ARCH := $(shell uname -m)
 
 ifneq (,$(filter $(ARCH),ppc64le ppc64))
 ARCH_PATH = ./powerpc
-ARCH_OBJS = $(ARCH_PATH)/power8.o $(ARCH_PATH)/plat.o $(ARCH_PATH)/util.o
+ARCH_OBJS = $(ARCH_PATH)/power8.o $(ARCH_PATH)/plat.o $(ARCH_PATH)/util.o \
+	$(ARCH_PATH)/ui_perf_map.o
 else
 ARCH_PATH = ./intel
 ARCH_OBJS = $(ARCH_PATH)/wsm.o $(ARCH_PATH)/snb.o $(ARCH_PATH)/nhm.o \
 	$(ARCH_PATH)/bdw.o $(ARCH_PATH)/skl.o $(ARCH_PATH)/plat.o \
-	$(ARCH_PATH)/util.o
+	$(ARCH_PATH)/util.o $(ARCH_PATH)/ui_perf_map.o
 endif
 
 all: $(PROG)

common/include/lwp.h
@@ -67,7 +67,7 @@ extern void lwp_enum_update(struct _track_proc *);
 extern int lwp_refcount_inc(track_lwp_t *);
 extern void lwp_refcount_dec(track_lwp_t *);
 extern int lwp_key_compute(track_lwp_t *, void *, boolean_t *end);
-extern int lwp_countval_update(track_lwp_t *, int, count_id_t, uint64_t);
+extern int lwp_countval_update(track_lwp_t *, int, perf_count_id_t, uint64_t);
 extern int lwp_intval_get(track_lwp_t *);
 extern void lwp_intval_update(struct _track_proc *, int intval_ms);
 extern void lwp_quitting_set(track_lwp_t *);

common/include/os/node.h
@@ -115,12 +115,12 @@ extern void node_group_unlock(void);
 extern node_t *node_by_cpu(int);
 extern int node_ncpus(node_t *);
 extern int node_intval_get(void);
-extern void node_countval_update(node_t *, count_id_t, uint64_t);
-extern uint64_t node_countval_get(node_t *, count_id_t);
+extern void node_countval_update(node_t *, perf_count_id_t, uint64_t);
+extern uint64_t node_countval_get(node_t *, ui_count_id_t);
 extern void node_meminfo(int, node_meminfo_t *);
 extern int node_cpu_traverse(pfn_perf_cpu_op_t, void *, boolean_t,
 	pfn_perf_cpu_op_t);
-extern uint64_t node_countval_sum(count_value_t *, int, int, count_id_t);
+extern uint64_t node_countval_sum(count_value_t *, int, int, ui_count_id_t);
 extern perf_cpu_t* node_cpus(node_t *);
 extern void node_intval_update(int);
 extern void node_profiling_clear(void);

common/include/os/os_perf.h
@@ -39,7 +39,7 @@ extern "C" {
 #endif
 
 #define PERF_REC_NUM	512
-#define PERF_FD_NUM		NCPUS_MAX * COUNT_NUM
+#define PERF_FD_NUM		NCPUS_MAX * PERF_COUNT_NUM
 #define INVALID_CODE_UMASK	(uint64_t)(-1)
 #define PERF_PQOS_CMT_MAX	10
 
@@ -61,7 +61,7 @@ typedef struct _os_perf_llrec {
 
 typedef struct _perf_cpu {
 	int cpuid;
-	int fds[COUNT_NUM];
+	int fds[PERF_COUNT_NUM];
 	int group_idx;
 	int map_len;
 	int map_mask;
@@ -99,7 +99,9 @@ extern boolean_t os_profiling_started(struct _perf_ctl *);
 extern int os_profiling_start(struct _perf_ctl *, union _perf_task *);
 extern int os_profiling_smpl(struct _perf_ctl *, union _perf_task *, int *);
 extern int os_profiling_partpause(struct _perf_ctl *, union _perf_task *);
+extern int os_profiling_multipause(struct _perf_ctl *, union _perf_task *);
 extern int os_profiling_restore(struct _perf_ctl *, union _perf_task *);
+extern int os_profiling_multi_restore(struct _perf_ctl *, union _perf_task *);
 extern int os_callchain_start(struct _perf_ctl *, union _perf_task *);
 extern int os_callchain_smpl(struct _perf_ctl *, union _perf_task *, int *);
 extern int os_ll_start(struct _perf_ctl *, union _perf_task *);
@@ -107,8 +109,10 @@ extern int os_ll_smpl(struct _perf_ctl *, union _perf_task *, int *);
 extern int os_perf_init(void);
 extern void os_perf_fini(void);
 extern void os_perfthr_quit_wait(void);
-extern int os_perf_profiling_partpause(count_id_t);
-extern int os_perf_profiling_restore(count_id_t);
+extern int os_perf_profiling_partpause(perf_count_id_t);
+extern int os_perf_profiling_multipause(perf_count_id_t *);
+extern int os_perf_profiling_restore(perf_count_id_t);
+extern int os_perf_profiling_multi_restore(perf_count_id_t *);
 extern int os_perf_callchain_start(pid_t, int);
 extern int os_perf_callchain_smpl(void);
 extern int os_perf_ll_smpl(struct _perf_ctl *, pid_t, int);

common/include/os/pfwrapper.h
@@ -74,7 +74,7 @@ extern "C" {
 #endif
 
 typedef struct _pf_conf {
-	count_id_t count_id;
+	perf_count_id_t perf_count_id;
 	uint32_t type;
 	uint64_t config;
 	uint64_t config1;
@@ -95,7 +95,7 @@ typedef struct _pf_profiling_rbrec {
 	uint32_t tid;
 	uint64_t time_enabled;
 	uint64_t time_running;
-	uint64_t counts[COUNT_NUM];
+	uint64_t counts[PERF_COUNT_NUM];
 	uint64_t ip_num;
 } pf_profiling_rbrec_t;
 
@@ -126,8 +126,8 @@ typedef int (*pfn_pf_event_op_t)(struct _perf_cpu *);
 
 int pf_ringsize_init(void);
 int pf_profiling_setup(struct _perf_cpu *, int, pf_conf_t *);
-int pf_profiling_start(struct _perf_cpu *, count_id_t);
-int pf_profiling_stop(struct _perf_cpu *, count_id_t);
+int pf_profiling_start(struct _perf_cpu *, perf_count_id_t);
+int pf_profiling_stop(struct _perf_cpu *, perf_count_id_t);
 int pf_profiling_allstart(struct _perf_cpu *);
 int pf_profiling_allstop(struct _perf_cpu *);
 void pf_profiling_record(struct _perf_cpu *, pf_profiling_rec_t *, int *);

common/include/os/plat.h
@@ -56,11 +56,11 @@ typedef struct _plat_event_config {
 	char desc[PLAT_EVENT_DESC_SIZE];
 } plat_event_config_t;
 
-extern uint64_t g_sample_period[COUNT_NUM][PRECISE_NUM];
+extern uint64_t g_sample_period[PERF_COUNT_NUM][PRECISE_NUM];
 extern cpu_type_t s_cpu_type;
 extern boolean_t g_cmt_enabled;
 
-typedef void (*pfn_plat_profiling_config_t)(count_id_t,
+typedef void (*pfn_plat_profiling_config_t)(perf_count_id_t,
     plat_event_config_t *);
 typedef void (*pfn_plat_ll_config_t)(plat_event_config_t *);
 typedef int (*pfn_plat_offcore_num_t)(void);
@@ -70,9 +70,9 @@ extern pfn_plat_ll_config_t s_plat_ll_config[CPU_TYPE_NUM];
 extern pfn_plat_offcore_num_t s_plat_offcore_num[CPU_TYPE_NUM];
 
 extern int plat_detect(void);
-extern void plat_profiling_config(count_id_t, plat_event_config_t *);
+extern void plat_profiling_config(perf_count_id_t, plat_event_config_t *);
 extern void plat_ll_config(plat_event_config_t *);
-extern void plat_config_get(count_id_t, plat_event_config_t *, plat_event_config_t *);
+extern void plat_config_get(perf_count_id_t, plat_event_config_t *, plat_event_config_t *);
 extern int plat_offcore_num(void);
 
 #ifdef __cplusplus

common/include/perf.h
@@ -46,6 +46,7 @@ typedef enum {
 	PERF_STATUS_IDLE = 0,
 	PERF_STATUS_PROFILING_STARTED,
 	PERF_STATUS_PROFILING_PART_STARTED,
+	PERF_STATUS_PROFILING_MULTI_STARTED,
 	PERF_STATUS_PROFILING_FAILED,
 	PERF_STATUS_CALLCHAIN_STARTED,
 	PERF_STATUS_CALLCHAIN_FAILED,
@@ -61,7 +62,9 @@ typedef enum {
 	PERF_INVALID_ID = 0,
 	PERF_PROFILING_START_ID,
 	PERF_PROFILING_PARTPAUSE_ID,
+	PERF_PROFILING_MULTIPAUSE_ID,
 	PERF_PROFILING_RESTORE_ID,
+	PERF_PROFILING_MULTI_RESTORE_ID,
 	PERF_PROFILING_SMPL_ID,
 	PERF_CALLCHAIN_START_ID,
 	PERF_CALLCHAIN_SMPL_ID,
@@ -92,13 +95,23 @@ typedef struct _task_profiling {
 
 typedef struct _task_partpause {
 	perf_taskid_t task_id;
-	count_id_t count_id;
+	perf_count_id_t perf_count_id;
 } task_partpause_t;
+
+typedef struct _task_multipause {
+	perf_taskid_t task_id;
+	perf_count_id_t *perf_count_ids;
+} task_multipause_t;
 
 typedef struct _task_restore {
 	perf_taskid_t task_id;
-	count_id_t count_id;
+	perf_count_id_t perf_count_id;
 } task_restore_t;
+
+typedef struct _task_multi_restore {
+	perf_taskid_t task_id;
+	perf_count_id_t *perf_count_ids;
+} task_multi_restore_t;
 
 typedef struct _task_callchain {
 	perf_taskid_t task_id;
@@ -156,7 +169,7 @@ typedef struct _perf_chainrecgrp {
 } perf_chainrecgrp_t;
 
 typedef struct _perf_countchain {
-	perf_chainrecgrp_t chaingrps[COUNT_NUM];
+	perf_chainrecgrp_t chaingrps[PERF_COUNT_NUM];
 } perf_countchain_t;
 
 #define	TASKID(task_addr) \
@@ -187,8 +200,8 @@ extern int perf_allstop(void);
 extern boolean_t perf_profiling_started(void);
 extern int perf_profiling_start(void);
 extern int perf_profiling_smpl(boolean_t);
-extern int perf_profiling_partpause(count_id_t);
-extern int perf_profiling_restore(count_id_t);
+extern int perf_profiling_partpause(ui_count_id_t);
+extern int perf_profiling_restore(ui_count_id_t);
 extern boolean_t perf_callchain_started(void);
 extern int perf_callchain_start(pid_t, int);
 extern int perf_callchain_smpl(void);

commit fe17a5d7b620e505f1830fa5f5d0a1ad40484a83

common/perf: Fix null pointer dereference
As pointed out by ColinlanKing[1], perf_profiling_partpause() and
perf_profiling_restore() functions uses pointer 'perf_count_ids'
without checking it for NULL.

Function get_ui_perf_count_map() returns without setting proper
value in 'perf_count_ids' when 'ui_count_id' is UI_COUNT_INVALID.
Right now, there are sufficient pre-checks already present before
calling perf_profiling_partpause() and perf_profiling_restore()
with ui_count_id = UI_COUNT_INVALID. So, user won't face any issue
without this fix. But it's still good to have a check there.

Add a condition to check for NULL pointer before using it.

[1] https://github.com/01org/numatop/issues/36

Reported-by: Colin Ian King <colin.king@canonical.com>
Signed-off-by: Ravi Bangoria <ravi.bangoria@linux.vnet.ibm.com>
Fixes: eb574e79c205 ("Support multiple perf event for one UI parameter")

common/perf.c
@@ -499,6 +499,10 @@ perf_profiling_partpause(ui_count_id_t ui_count_id)
 	int n_perf_count;
 
 	n_perf_count = get_ui_perf_count_map(ui_count_id, &perf_count_ids);
+
+	if (n_perf_count == PERF_COUNT_INVALID)
+		return (-1);
+
 	if (n_perf_count > 1) {
 		return (os_perf_profiling_multipause(perf_count_ids));
 	} else {
@@ -513,6 +517,10 @@ perf_profiling_restore(ui_count_id_t ui_count_id)
 	int n_perf_count;
 
 	n_perf_count = get_ui_perf_count_map(ui_count_id, &perf_count_ids);
+
+	if (n_perf_count == PERF_COUNT_INVALID)
+		return (-1);
+
 	if (n_perf_count > 1) {
 		return (os_perf_profiling_multi_restore(perf_count_ids));
 	} else  {