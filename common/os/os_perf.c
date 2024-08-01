intel/numatop

commit 8a4a6c57a74df11d81633cd6d4a5ee2b7812563f

Add a feature to show per-Node QPI bandwidth
Signed-off-by: Jin Yao <yao.jin@linux.intel.com>

common/os/os_perf.c
@@ -841,6 +841,21 @@ os_perf_countchain_reset(perf_countchain_t *count_chain)
 	memset(count_chain, 0, sizeof (perf_countchain_t));
 }
 
+static int
+uncoreqpi_stop_all(void)
+{
+	node_t *node;
+	int i;
+
+	for (i = 0; i < NNODES_MAX; i++) {
+		node = node_get(i);
+		if (NODE_VALID(node) && (node->qpi.qpi_num > 0))
+			pf_uncoreqpi_free(node);
+	}
+
+	return 0;
+}
+
 void
 os_allstop(void)
 {
@@ -854,6 +869,10 @@ os_allstop(void)
 
 	if (perf_pqos_cmt_started()) {
 		proc_pqos_func(NULL, os_pqos_cmt_proc_free);
+	}
+
+	if (perf_uncoreqpi_started()) {
+		uncoreqpi_stop_all();
 	}
 }
 
@@ -1236,5 +1255,106 @@ int os_pqos_proc_stop(perf_ctl_t *ctl, perf_task_t *task)
 		proc_refcount_dec(proc);
 	}
 
+	return (0);
+}
+
+int os_uncoreqpi_stop(perf_ctl_t *ctl, perf_task_t *task)
+{
+	task_uncoreqpi_t *t = (task_uncoreqpi_t *)task;
+	node_t *node;
+	int i;
+	
+	if (t->nid >= 0) {
+		node = node_get(t->nid);
+		if (NODE_VALID(node) && (node->qpi.qpi_num > 0))
+			pf_uncoreqpi_free(node);
+	} else {
+		for (i = 0; i < NNODES_MAX; i++) {
+			node = node_get(i);
+			if (NODE_VALID(node) && (node->qpi.qpi_num > 0))
+				pf_uncoreqpi_free(node);
+		}
+	}
+
+	return 0;
+}
+
+static int uncoreqpi_start(perf_ctl_t *ctl, int nid)
+{
+	node_t *node;
+	
+	node = node_get(nid);
+	if (!NODE_VALID(node))
+		return -1;
+	
+	if (pf_uncoreqpi_setup(node) != 0)
+		return -1;
+	
+	if (pf_uncoreqpi_start(node) != 0)
+		return -1;
+	
+	return 0;
+}
+
+int
+os_uncoreqpi_start(perf_ctl_t *ctl, perf_task_t *task)
+{
+	task_uncoreqpi_t *t = (task_uncoreqpi_t *)task;
+
+	if (uncoreqpi_start(ctl, t->nid) != 0) {
+		debug_print(NULL, 2,
+			"uncoreqpi_start is failed for node %d/%d\n",
+			t->nid);
+		perf_status_set(PERF_STATUS_UNCOREQPI_FAILED);
+		return (-1);
+	}
+
+	perf_status_set(PERF_STATUS_UNCOREQPI_STARTED);
+	return (0);
+}
+
+int
+os_uncoreqpi_smpl(perf_ctl_t *ctl, perf_task_t *task, int *intval_ms)
+{
+	task_uncoreqpi_t *t = (task_uncoreqpi_t *)task;
+	node_t *node;
+	int ret;
+
+	node = node_get(t->nid);
+	if (!NODE_VALID(node))
+		return -1;
+
+	ret = pf_uncoreqpi_smpl(node);
+
+	if (ret == 0)
+		disp_profiling_data_ready(*intval_ms);
+	else
+		disp_profiling_data_fail();
+
+	return (ret);
+}
+
+boolean_t
+os_perf_uncoreqpi_started(perf_ctl_t *ctl)
+{
+	if (ctl->status == PERF_STATUS_UNCOREQPI_STARTED)
+		return (B_TRUE);
+
+	return (B_FALSE);
+}
+
+int
+os_perf_uncoreqpi_smpl(perf_ctl_t *ctl, int nid)
+{
+	perf_task_t task;
+	task_uncoreqpi_t *t;
+
+	perf_smpl_wait();
+	memset(&task, 0, sizeof (perf_task_t));
+	t = (task_uncoreqpi_t *)&task;
+	t->task_id = PERF_UNCOREQPI_SMPL_ID;
+	t->nid = nid;
+
+	perf_task_set(&task);
 	return (0);
 }

common/cmd.c
@@ -134,6 +134,12 @@ preop_switch2pqosmbm(cmd_t *cmd, boolean_t *smpl)
 	return (os_preop_switch2pqosmbm(cmd, smpl));
 }
 
+static int
+preop_switch2uncoreqpi(cmd_t *cmd, boolean_t *smpl)
+{
+	return (os_preop_switch2uncoreqpi(cmd, smpl));
+}
+
 int
 op_page_next(cmd_t *cmd, boolean_t smpl)
 {	
@@ -419,7 +425,15 @@ switch_table_init(void)
 	s_switch[WIN_TYPE_NODE_OVERVIEW][CMD_NODE_OVERVIEW_ID].preop = NULL;
 	s_switch[WIN_TYPE_NODE_OVERVIEW][CMD_NODE_OVERVIEW_ID].op = NULL;
 	s_switch[WIN_TYPE_NODE_OVERVIEW][CMD_BACK_ID].op = op_page_prev;
+	s_switch[WIN_TYPE_NODE_OVERVIEW][CMD_NODE_DETAIL_ID].preop =
+		preop_switch2uncoreqpi;	
 	s_switch[WIN_TYPE_NODE_OVERVIEW][CMD_NODE_DETAIL_ID].op = op_page_next;
+
+	/*
+	 * Initialize for window type "WIN_TYPE_NODE_DETAIL"
+	 */
+	s_switch[WIN_TYPE_NODE_DETAIL][CMD_BACK_ID].preop =
+		preop_switch2profiling;
 
 	/*
 	 * Initialize for window type "WIN_TYPE_CALLCHAIN"

common/include/cmd.h
@@ -82,7 +82,7 @@ typedef enum {
 	CMD_BACK_ID,
 	CMD_RESIZE_ID,
 	CMD_PQOS_CMT_ID,
-	CMD_PQOS_MBM_ID
+	CMD_PQOS_MBM_ID,
 } cmd_id_t;
 
 #define CMD_NUM	25
@@ -223,6 +223,9 @@ typedef struct _switch {
 
 #define	CMD_PQOS_MBM(cmd) \
 	((cmd_pqos_mbm_t *)(cmd))
+
+#define	CMD_UNCOREQPI(cmd) \
+	((cmd_uncoreqpi_t *)(cmd))
 
 extern int g_sortkey;
 

common/include/os/node.h
@@ -44,6 +44,8 @@ extern "C" {
 #define INVALID_CPUID	-1
 #define	NODE_VALID(node) ((node)->nid != INVALID_NID)
 
+#define NODE_QPI_MAX	3
+
 typedef struct _node_meminfo {
 	uint64_t mem_total;
 	uint64_t mem_free;
@@ -54,12 +56,26 @@ typedef struct _node_meminfo {
 	uint64_t mapped;
 } node_meminfo_t;
 
+typedef struct _qpi_info {
+	int id;
+	int type;
+	int fd;
+	uint64_t values[3];
+	uint64_t value_scaled;
+} qpi_info_t;
+
+typedef struct _node_qpi {
+	int qpi_num;
+	qpi_info_t qpi_info[NODE_QPI_MAX];
+} node_qpi_t;
+
 typedef struct _node {
 	int nid;
 	int ncpus;
 	perf_cpu_t cpus[NCPUS_NODE_MAX];
 	count_value_t countval;
 	node_meminfo_t meminfo;
+	node_qpi_t qpi;
 	boolean_t hotadd;
 	boolean_t hotremove;	
 } node_t;
@@ -94,6 +110,7 @@ extern void node_intval_update(int);
 extern void node_profiling_clear(void);
 extern node_t* node_valid_get(int);
 extern int node_cpuid_max(void);
+extern int node_qpi_init(void);
 
 #ifdef __cplusplus
 }

common/include/os/os_cmd.h
@@ -51,6 +51,7 @@ extern int os_preop_switch2accdst(cmd_t *, boolean_t *);
 extern int os_preop_leavecallchain(cmd_t *, boolean_t *);
 extern int os_preop_switch2pqoscmt(cmd_t *, boolean_t *);
 extern int os_preop_switch2pqosmbm(cmd_t *, boolean_t *);
+extern int os_preop_switch2uncoreqpi(cmd_t *, boolean_t *);
 
 extern int os_op_llmap_stop(cmd_t *, boolean_t);
 extern int os_op_lnmap_stop(cmd_t *, boolean_t);

common/include/os/os_perf.h
@@ -135,6 +135,11 @@ extern int os_pqos_cmt_lwp_smpl(struct _track_lwp *, void *, boolean_t *);
 extern int os_pqos_cmt_proc_free(struct _track_proc *, void *, boolean_t *);
 extern boolean_t os_perf_pqos_cmt_started(struct _perf_ctl *);
 extern int os_pqos_proc_stop(struct _perf_ctl *, union _perf_task *);
+extern int os_uncoreqpi_stop(struct _perf_ctl *, union _perf_task *);
+extern int os_uncoreqpi_start(struct _perf_ctl *, union _perf_task *);
+extern int os_uncoreqpi_smpl(struct _perf_ctl *, union _perf_task *, int *);
+extern boolean_t os_perf_uncoreqpi_started(struct _perf_ctl *);
+extern int os_perf_uncoreqpi_smpl(struct _perf_ctl *, int);
 
 #ifdef __cplusplus
 }

common/include/os/os_util.h
@@ -30,6 +30,7 @@
 #define	_NUMATOP_OS_UTIL_H
 
 #include <sys/types.h>
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <inttypes.h>
@@ -72,6 +73,7 @@ extern int sysfs_os_online_ncpus(void);
 extern boolean_t os_sysfs_meminfo(int, node_meminfo_t *);
 extern int os_sysfs_online_ncpus(void);
 extern int os_sysfs_cqm_llc_scale(const char*, double *);
+extern int os_sysfs_uncore_qpi_init(qpi_info_t *, int);
 
 #ifdef __cplusplus
 }

common/include/os/pfwrapper.h
@@ -111,6 +111,7 @@ extern precise_type_t g_precise;
 
 struct _perf_cpu;
 struct _perf_pqos;
+struct _node;
 
 typedef int (*pfn_pf_event_op_t)(struct _perf_cpu *);
 
@@ -133,6 +134,10 @@ int pf_pqos_start(struct _perf_pqos *);
 int pf_pqos_stop(struct _perf_pqos *);
 void pf_pqos_record(struct _perf_pqos *);
 void pf_pqos_resource_free(struct _perf_pqos *);
+void pf_uncoreqpi_free(struct _node *);
+int pf_uncoreqpi_setup(struct _node *);
+int pf_uncoreqpi_start(struct _node *);
+int pf_uncoreqpi_smpl(struct _node *);
 
 #ifdef __cplusplus
 }

commit 41db077a4c36e6f28ea55c5df7d6f34c886d5a0a

common: remove extra %d from debug_print and fix grammar
The debug_print print formats two integers using %d/%d but only
one integer is being passed as an argument. Remove the send %d.
Also fix grammar in the debug message.

Fixes: 8a4a6c57a74d ("Add a feature to show per-Node QPI bandwidth")
Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

common/os/os_perf.c
@@ -1510,7 +1510,7 @@ os_uncore_start(perf_ctl_t *ctl, perf_task_t *task)
 
 	if (uncore_start(ctl, t->nid) != 0) {
 		debug_print(NULL, 2,
-			"os_uncore_start is failed for node %d/%d\n",
+			"os_uncore_start failed for node %d\n",
 			t->nid);
 		perf_status_set(PERF_STATUS_UNCORE_FAILED);
 		return (-1);